28581c76ad2157db1406d5611bc9cdf50026f24a
Sumit Sidharth <sumitsidharth30@gmail.com>
2026-03-14 20:44:26 +0530

    fix all

--- p1/Django/Portfolio_project/firstapp/models.py
+++ p1/Django/Portfolio_project/firstapp/models.py
@@ -1,11 +1,21 @@
 from django.db import models
 
-# Create your models here.
-class Contact(models.Model):
-    name= models.CharField(max_length=40)
-    email=models.EmailField(max_length=40)
-    content=models.TextField(max_length=400)
-    number=models.CharField(max_length=10)
 
-    # def __self__(self):
-    #     return(self.name)
+class Contact(models.Model):
+    """
+    Model for storing contact form submissions.
+    """
+    name = models.CharField(max_length=40)
+    email = models.EmailField(max_length=40)
+    content = models.TextField(max_length=400)
+    number = models.CharField(max_length=12)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['-date']
+        verbose_name = 'Contact Message'
+        verbose_name_plural = 'Contact Messages'
+
--- p1/Django/Portfolio_project/firstapp/views.py
+++ p1/Django/Portfolio_project/firstapp/views.py
@@ -1,48 +1,54 @@
-from django.shortcuts import render ,redirect
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib import messages
 from firstapp import models
-from firstapp.models import Contact 
-# from django.contrib.auth.decorators import login_required
-# def home(request):
-#     return render(request,'home.html')
+from firstapp.models import Contact
 
 
-# @login_required(login_url='')
+def home(request):
+    """
+    Render the home page.
+    """
+    return render(request, 'home.html')
+
+
 def contact(request):
-   if request.method=="POST":
-       print('post')
-       name=request.POST.get('name')
-       email=request.POST.get('email')
-       number=request.POST.get('number')
-       content=request.POST.get('content')
-       print(name,email,number,content )
+    """
+    Handle contact form submissions.
+    Validates input and saves contact information to database.
+    """
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        number = request.POST.get('number')
+        content = request.POST.get('content')
 
-       if len(name)>1 and len(name)<30:
-           pass
-       else:
-           messages.error(request,'Lenght of name should be greater than 2 and less than 30 words ')
-           return render(request,'home.html')
-       
-       if len (email)>1 and len(email)<30:
-           pass
-       else:
-           messages.error(request,'invaild email try again ')
-           return render(request,'home.html')
-       print(len(number))
-       if  len(number)>9 and len(number)<13:
-           pass
-       else:
-           messages.error(request,'invaild number please try again ')
-           return render(request,'home.html')
-       ins = models.Contact(name=name,email=email,content=content,number=number)
-       ins.save()
-       messages.success(request,'Thank You for contacting me!! Your message has been saved ')
-       print('data has been saved to database')
- 
-       print('The request is no pass ')
-   return render(request,'home.html') 
+        # Validate name
+        if not (1 < len(name) < 30):
+            messages.error(request, 'Length of name should be greater than 1 and less than 30 characters')
+            return render(request, 'home.html')
 
+        # Validate email
+        if not (1 < len(email) < 50):
+            messages.error(request, 'Invalid email, please try again')
+            return render(request, 'home.html')
 
-  
-  
+        # Validate phone number (should be 10-12 digits)
+        if not (9 < len(number) < 13):
+            messages.error(request, 'Invalid phone number, please try again')
+            return render(request, 'home.html')
+
+        # Save contact to database
+        ins = models.Contact(
+            name=name,
+            email=email,
+            content=content,
+            number=number
+        )
+        ins.save()
+
+        messages.success(request, 'Thank you for contacting me!! Your message has been saved')
+        print('Data has been saved to database')
+
+    return render(request, 'home.html')
+
